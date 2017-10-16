c246f317f3684e97265d15992fac81d7ad5f890c
Diana <diana.olsen@colorado.edu>
2017-10-16 14:11:34 -0600

    First commit

---
+++ src/main/java/org/dsouthard/webapp/repository/GreetingRepository.java
@@ -0,0 +1,10 @@
+package org.dsouthard.webapp.repository;
+
+import org.dsouthard.webapp.model.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+
+}
